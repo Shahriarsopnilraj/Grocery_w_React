c73abc9a2ff791a4bd6889815fac6415cd425369
Shahriar <shahriarsopnilraj@gmail.com>
2025-11-10 21:22:34 +0600

    Components

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = (props) => {
   return (
     <div className=" mx-auto w-fit">
-      <h2 className=" md:text-5xl text-[2.5rem] font-bold">
+      <h2 className=" md:text-5xl text-zinc-800 text-[2.5rem] font-bold">
         <span className="text-orange-500">{props.highlight}</span> {props.heading}
       </h2>
       <div className="w-30 md:mt-6 mt-3 h-1 bg-orange-300 ml-auto"> </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,9 @@ import Category from '../Category/Category';
 import Values from '../Values/Values';
 import Products from '../Products/Products';
 import Discount from '../Discount/Discount';
+import Process from '../Process/Process';
+import Testimonials from '../Testimonials/Testimonials';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
   return (
@@ -15,6 +18,9 @@ const Home = () => {
       <Values/>
       <Products/>
       <Discount/>
+      <Process/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
